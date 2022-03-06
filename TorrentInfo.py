79a2b7cd62c0bb05f45e05a1321033775413fbe0
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2022-03-06 23:15:04 +0100

    format torrent title string

---
+++ TorrentInfo.py
@@ -0,0 +1,140 @@
+import re
+import imdb
+
+def format_torrent_name(torrent_name):
+    string = torrent_name 
+    string = string.replace(".", " ")
+    string = string.replace("-", " ")
+    string = re.sub(r"[\([{})\]]", "", string)
+    string = re.sub(r"[\([{})\]]", "", string)
+
+    # Get relese year from title string
+    year = None
+    search=re.search('20[0-9]{2} | 19[5-9][0-9]', string)
+
+    idx = len(string) # Used to determine length of title 
+    if search: 
+        year = search.group()
+        idx = search.span()[0]
+
+    # Get season information from title string  
+    season = None
+    season_formats = [" Season [0-9]+", " SEASON [0-9]+", " S[0-9]+", "s[0-9]"]
+    for s in season_formats:
+        search = re.search(s, string)
+        if search:
+            season = search.group() 
+            season = re.search('[0-9]+', season).group()
+            check_idx = search.span()[0]
+            if check_idx < idx:
+                idx = check_idx
+            break
+
+    # Get season information from title string  
+    #episode = ""
+    episode = None
+    episode_formats = [" Episode [0-9]+", " EPISODE [0-9]+", " E[0-9]+", "e[0-9]"]
+    for e in episode_formats:
+        search = re.search(e, string)
+        if search:
+            episode = search.group() 
+            episode = re.search('[0-9]+', episode).group()
+            check_idx = search.span()[0]
+            if check_idx < idx:
+                idx = check_idx
+            break
+
+    # Used idx to determine name 
+    name = string[:idx]
+
+    # Return dict with info 
+    #info.titles = name_list
+    info = {}
+    info["title"] = string[:idx] 
+    info["release"] = year
+    info["season"] = season
+    info["episode"] = episode
+    return info
+
+def get_rating(title): 
+    ia = imdb.Cinemagoer()
+    series = ia.search_movie(title)
+    assert(len(series)!=0)
+    #series = ia.search_movie('madalorian')
+
+    #series_id = series[0].movieID
+
+    print(len(series))
+
+def get_plot(title:str, season:int, episode:int):
+    ia = imdb.Cinemagoer()
+    series = ia.search_movie(title)
+    series_id = series[0].movieID
+    series = ia.get_movie(series_id)
+    ia.update(series, 'episodes')
+    print(series['episodes'].keys())
+    print(series['episodes'][season][episode])
+
+def format_titles(titles:list):
+    name_list = []
+    year_list = []
+    season_list = []
+    episode_list = []
+    #info = {"titles":None, "release":None, "season":None, "episode":None}
+    info = {}
+    for string in titles:
+        string = string.replace(".", " ")
+        string = string.replace("-", " ")
+        string = re.sub(r"[\([{})\]]", "", string)
+        string = re.sub(r"[\([{})\]]", "", string)
+
+        # Get relese year from title string
+        year = ""
+        search=re.search('20[0-9]{2} | 19[5-9][0-9]', string)
+
+        idx = len(string) # Used to determine length of title 
+        if search: 
+            year = search.group()
+            idx = search.span()[0]
+        year_list.append(year)
+
+        # Get season information from title string  
+        season = ""
+        season_formats = [" Season [0-9]+", " SEASON [0-9]+", " S[0-9]+", "s[0-9]"]
+        for s in season_formats:
+            search = re.search(s, string)
+            if search:
+                season = search.group() 
+                check_idx = search.span()[0]
+                if check_idx < idx:
+                    idx = check_idx
+                break
+        season_list.append(season)
+
+        # Get season information from title string  
+        episode = ""
+        episode_formats = [" Episode [0-9]+", " EPISODE [0-9]+", " E[0-9]+", "e[0-9]"]
+        for e in episode_formats:
+            search = re.search(e, string)
+            if search:
+                episode = search.group() 
+                check_idx = search.span()[0]
+                if check_idx < idx:
+                    idx = check_idx
+                break
+        episode_list.append(episode)
+
+        # Used idx to determine name 
+        name_list.append(string[:idx])
+
+    # Return dict with info 
+    #info.titles = name_list
+    info["title"] = name_list
+    info["release"] = year_list
+    info["season"] = season_list
+    info["episode"] = episode_list
+    return info
+    
+
+
+
